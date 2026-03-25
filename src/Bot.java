e45fe256aaaf218057f93c596126a4f7face8215
68070174-tech <68070174@kmitl.ac.th>
2026-03-25 20:53:36 +0700

    Merge conflict fixed

--- src/Bot.java
+++ src/Bot.java
@@ -1,8 +1,11 @@
 
+import java.util.Random;
 public class Bot extends Player {
+    private int effectUsed = 0;
+    private final int MAX_EFFECT = 2;
 
-    public Bot() {
-        for (int i = 1; i <= 5; i++) {
+    public Bot(){
+        for(int i = 1;i <= 5; i++){
             cardList.add(new Card(i));
         }
     }
@@ -21,7 +24,30 @@ public class Bot extends Player {
     }
 
     @Override
-    public Card selectMove() {
-        return cardList.remove(0);
-    }
-}
+    public Card selectMove(){
+        Random rand = new Random();
+        int index = rand.nextInt(cardList.size());
+        String effect;
+        if(effectUsed < MAX_EFFECT){
+            String[] effects = {"none", "negative", "zero"};
+            effect = effects[rand.nextInt(3)];
+        } else {
+            effect = "none";
+        }
+        Card using ;
+        if(effect.equals("negative")){
+            using = cardList.remove(index);
+            using.setType(new Negative());
+            effectUsed++;
+        }
+        else if(effect.equals("zero")){
+            using = cardList.remove(index);
+            using.setType(new Zero());
+            effectUsed++;
+        }
+        else{
+            using = cardList.remove(index);
+        }
+        return using;
+   }
+} 
