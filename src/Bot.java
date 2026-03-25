f1817d25eb08a1a8d2cef4588c63d9c7ee6fb185
68070122 <68070122@kmitl.ac.th>
2026-03-25 19:28:20 +0700

    Bot.java
    add logic v1

--- src/Bot.java
+++ src/Bot.java
@@ -1,10 +1,14 @@
+import java.util.Random;
 public class Bot extends Player {
+    private int effectUsed = 0;
+    private final int MAX_EFFECT = 2;
+
     public Bot(){
         for(int i = 1;i <= 5; i++){
             cardList.add(new Card(i));
         }
     }
-    
+
     @Override
     public boolean buyItem(EffectCard c){
         return true;
@@ -12,6 +16,29 @@ public class Bot extends Player {
     
     @Override
     public Card selectMove(){
-        return cardList.remove(0);
-    }
-}
+        Random rand = new Random();
+        int index = rand.nextInt(cardList.size());
+        String effect;
+        if(effectUsed < MAX_EFFECT){
+            String[] effects = {"none", "negative", "zero"};
+            effect = effects[rand.nextInt(3)];
+        } else {
+            effect = "none";
+        }
+        Card using ;
+        if(effect.equals("negative")){
+            using = cardList.remove(index);
+            using.setType(new Negative());
+            effectUsed++;
+        }
+        else if(effect.equals("zero")){
+            using = cardList.remove(index);
+            using.setType(new Zero());
+            effectUsed++;
+        }
+        else{
+            using = cardList.remove(index);
+        }
+        return using;
+   }
+} 
