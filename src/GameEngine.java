fb941b638b98a31b3fe7ca0d315041bf94b8a3d1
Tian2321 <paridinpitak235@gmail.com>
2026-03-26 19:45:02 +0700

    socket

--- src/GameEngine.java
+++ src/GameEngine.java
@@ -1,89 +1,121 @@
+
 import java.util.*;
-public class GameEngine implements Runnable{
+
+public class GameEngine implements Runnable {
+
     private final ArrayList<Player> playerList;
     private int cumulativeTotal;
     private boolean zero;
-    
-    public GameEngine(){
+    private GameServer server; // null = offline mode
+
+    // offline mode
+    public GameEngine() {
         playerList = new ArrayList<>();
         playerList.add(new Bot());
         playerList.add(new Bot());
         playerList.add(new Human());
-        
+        init();
+    }
+
+    // network mode
+    public GameEngine(List<Player> players, GameServer server) {
+        playerList = new ArrayList<>(players);
+        this.server = server;
+        init();
+    }
+
+    private void init() {
         zero = false;
         Random rand = new Random();
-        for(Player player: playerList){
-            player.setTarget(0, rand.nextInt(0, 11));
-            player.setTarget(1, rand.nextInt(10, 21));   
+        for (Player p : playerList) {
+            p.setTarget(0, rand.nextInt(0, 11));
+            p.setTarget(1, rand.nextInt(10, 21));
         }
-        for(int i = 0; i < playerList.size(); i++){
-            System.out.print("Target of Player " + (i + 1) + " = " + playerList.get(i).getTarget()[0] + " and " + playerList.get(i).getTarget()[1] + "\n");
+        for (int i = 0; i < playerList.size(); i++) {
+            System.out.println("Target of Player " + (i + 1)
+                    + " = " + playerList.get(i).getTarget()[0]
+                    + " and " + playerList.get(i).getTarget()[1]);
         }
     }
-    
-    public void setZero(){
+
+    public void setZero() {
         cumulativeTotal = 0;
         zero = true;
     }
-    
-    public void addTotal(int score){
-        if(zero == false){
+
+    public void addTotal(int score) {
+        if (!zero) {
             cumulativeTotal += score;
         }
     }
-    
-    public void calculateEffect(Player p, Card c, EffectCard efc){
-        if(efc == null){
+
+    public void calculateEffect(Player p, Card c, EffectCard efc) {
+        if (efc == null) {
             addTotal(c.getValue());
             return;
         }
         efc.applyEffect(this, p, c);
     }
-    
-    public void addScore(ArrayList<Player> p){
-        p.get(0).addScore(2);
-        System.out.println("Player " + (playerList.indexOf(p.get(0)) + 1) + " got 2 points");
-        p.get(1).addScore(1);
-        System.out.println("Player " + (playerList.indexOf(p.get(1)) + 1)+ " got 1 points");
-        System.out.println("");
+
+    public void addScore(ArrayList<Player> sorted) {
+        sorted.get(0).addScore(2);
+        System.out.println("Player " + (playerList.indexOf(sorted.get(0)) + 1) + " got 2 points");
+        sorted.get(1).addScore(1);
+        System.out.println("Player " + (playerList.indexOf(sorted.get(1)) + 1) + " got 1 point");
+        System.out.println();
     }
-    
-    public void calculateScore(){
-        for(Player p: playerList){
-            //Find mininum difference from two targets and set to player's diif.
-            p.diff = Math.min(Math.abs(cumulativeTotal - p.getTarget()[0]), Math.abs(cumulativeTotal - p.getTarget()[1]));
+
+    public void calculateScore() {
+        for (Player p : playerList) {
+            p.diff = Math.min(
+                    Math.abs(cumulativeTotal - p.getTarget()[0]),
+                    Math.abs(cumulativeTotal - p.getTarget()[1]));
         }
-        ArrayList<Player> scoringList = new ArrayList<>(playerList);
-        //Sort player by least diff
-        scoringList.sort(Comparator.comparing(Player::getDiff));
-        addScore(scoringList);
+        ArrayList<Player> sorted = new ArrayList<>(playerList);
+        sorted.sort(Comparator.comparing(Player::getDiff));
+        addScore(sorted);
     }
-    
-    public void calculateRound(){
-        for(Player p: playerList){
-            Card calculating = p.selectMove();
-            calculateEffect(p, calculating, calculating.getType());
+
+    public void calculateRound(int roundNum) {
+        cumulativeTotal = 0;
+        StringBuilder log = new StringBuilder("--- Round " + roundNum + " ---\n");
+        for (Player p : playerList) {
+            Card c = p.selectMove();
+            calculateEffect(p, c, c.getType());
             p.addMoney(5);
         }
         zero = false;
-        System.out.println("Total points on the table = " + cumulativeTotal);
+        log.append("Total on table = ").append(cumulativeTotal).append("\n");
+        System.out.println(log);
         calculateScore();
+
+        if (server != null) {
+            int[] allScores = playerList.stream().mapToInt(Player::getScore).toArray();
+            server.broadcastState(
+                    GameMessage.roundResult(cumulativeTotal, allScores, log.toString()));
+        }
     }
-    
+
     @Override
-    public void run(){
-        for(int i = 0; i < 5; i++){
-            calculateRound();
+    public void run() {
+        for (int i = 1; i <= 5; i++) {
+            calculateRound(i);
         }
-        System.out.println("");
-        System.out.println("Total score:");
-        for(int i = 0; i < playerList.size(); i++){
-            System.out.println("Player " + (i + 1) + " got " + playerList.get(i).getScore() + " points");
+
+        System.out.println("\nTotal score:");
+        String[] names = new String[playerList.size()];
+        int[] finalScores = new int[playerList.size()];
+        for (int i = 0; i < playerList.size(); i++) {
+            names[i] = "Player " + (i + 1);
+            finalScores[i] = playerList.get(i).getScore();
+            System.out.println(names[i] + " got " + finalScores[i] + " points");
+        }
+        if (server != null) {
+            server.broadcastState(GameMessage.gameOver(names, finalScores));
         }
     }
-    
+
     public static void main(String[] args) {
-        Thread t = new Thread(new GameEngine());
-        t.start();
+        new Thread(new GameEngine()).start();
     }
 }
