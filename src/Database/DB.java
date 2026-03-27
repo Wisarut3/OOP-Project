33a46802f0d2d8d9aa4a7100e797e8e2c99a7913
68070174-tech <68070174@kmitl.ac.th>
2026-03-27 19:23:43 +0700

    Connecting database with GUI

--- src/Database/DB.java
+++ src/Database/DB.java
@@ -1,10 +1,14 @@
+package Database;
 import java.sql.*;
 
 public class DB {
     public static Connection getConnection() {
         try {
+            String url = "jdbc:mysql://root:WbDIAdoHGvuSDMQYzsdgdGLKQNiSyKll@gondola.proxy.rlwy.net:34173/railway";
+            String user = "root";
+            String pass = "WbDIAdoHGvuSDMQYzsdgdGLKQNiSyKll";
             Class.forName("com.mysql.cj.jdbc.Driver");
-            return DriverManager.getConnection("jdbc:mysql://localhost:3306/user_db", "root", "");
+            return DriverManager.getConnection(url, user, pass);
         } catch (Exception e) {
             e.printStackTrace();
             return null;
